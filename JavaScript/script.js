874572706bb642d7cb80965c27044479fadd4f53
overnek <augusto.overnek@gmail.com>
2024-04-04 16:42:41 -0300

    Comeco do menu de perfil

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -21,3 +21,21 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+//MENU DO PERFIL E ABA PERFIL
+
+let btnMenu = document.getElementById("btn-menu-perfil");
+let menu = document.getElementById("menu-perfil");
+let overlay = document.getElementById("overlay-menu");
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
