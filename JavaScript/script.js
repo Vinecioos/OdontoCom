cfb4b611e72876a41c00a516196004439c856d9e
Vinecioos <vinicius.bueno@live.com>
2024-04-10 09:24:42 -0300

    Alteações nos botões para A e correção o JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,26 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let botao1 = document.getElementById("botao1");
-    let botao2 = document.getElementById("botao2");
-    
-    botao1.addEventListener("click", function() {
-        let posicaoY = 650; 
-
-        window.scrollTo({
-            top: posicaoY,
-            behavior: 'smooth'
-        });
-    });
-
-    botao2.addEventListener("click", function() {
-        let posicaoY = 1200; 
-
-        window.scrollTo({
-            top: posicaoY,
-            behavior: 'smooth'
-        });
-    });
-});
-
 //MENU DO PERFIL E ABA PERFIL
 
 let btnMenu = document.getElementById("btn-menu-perfil");
