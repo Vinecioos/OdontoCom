31d3e85677261fc86dd22cf2cd3efe5ce40e580c
GLibanio <guilhermelibanio1@gmail.com>
2024-04-14 20:41:47 -0300

    Validação do Email

--- HTML/cadastro.php
+++ HTML/cadastro.php
@@ -1,26 +1,38 @@
 <?php
-    $dbHost = "Localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "login";
+$dbHost = "Localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "login";
 
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-    $email = $_POST['email'];
+function ValidaEmail($email)
+{
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+$email = $_POST['email'];
+
+if (!ValidaEmail($email)) {
+    echo "<script>alert('Email inválida!');";
+    echo "window.location='cadastro.html';</script>";
+} else {
     $sql = mysqli_query($conexao, "SELECT * FROM user WHERE email = '{$email}'");
 
-    if(mysqli_num_rows($sql)>0) {
+    if (mysqli_num_rows($sql) > 0) {
         echo "<script>alert('Já existe um usuário cadastrado com este email');";
         echo "window.location='cadastro.html';</script>";
-    } 
-    else {
+    } else {
         $sqlemail = "SELECT * FROM user WHERE email = '$email'";
 
-        $result = $conexao -> query($sqlemail);
-        
-        if(mysqli_num_rows($result) < 1){
-            if($_POST['password'] === $_POST['confirmed_password']){
+        $result = $conexao->query($sqlemail);
+
+        if (mysqli_num_rows($result) < 1) {
+            if ($_POST['password'] === $_POST['confirmed_password']) {
                 $email = $_POST['email'];
                 $senha = $_POST['password'];
                 $nome = $_POST['nome'];
@@ -30,19 +42,16 @@
                 $codigoSeg = strtoupper(substr(str_replace('-', '', uniqid()), 0, 6));
 
                 $result = mysqli_query($conexao, "INSERT INTO user(email, senha, nome, telefone, CPF, codigoSeg) VALUES ('$email', '$senha', '$nome','$telefone','$cpf','$codigoSeg')");
-        
+
                 echo "<script>alert('Cadastro realizado com sucesso! ANOTE SEU CODIGO DE SEGURANÇA: $codigoSeg');";
                 echo "window.location='login.html';</script>";
-            }
-            else{
+            } else {
                 echo "<script>alert('Senha inválida!');";
                 echo "window.location='cadastro.html';</script>";
             }
-        }
-        else{
+        } else {
             echo "<script>alert('email já cadastrado!');";
             echo "window.location='login.html';</script>";
         }
-        
     }
-?>
+}
